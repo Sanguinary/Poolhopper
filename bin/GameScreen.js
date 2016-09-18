112c52b88b3c8fe8d8d64914131e5d981b3f0991
Mike <mike42092@gmail.com>
2016-09-17 21:09:52 -0400

    Added onto NPCPerson move function to chase player, created array of NPCs in Game screen, deleted some old testing code

--- bin/GameScreen.js
+++ bin/GameScreen.js
@@ -9,16 +9,25 @@ app.GameScreen = {
 		//console.log(this.testButton);
 		//stage.addChild(this.testButton);
 		this.player = new app.Player();
-		this.npc = new app.NPC();
+		app.people = [];
+
+		//Create array of npcs
+		for(var i = 0; i < 5; i++){
+			app.people.push(new app.NPC());
+			app.people[i].graphic.position.y += i*100;
+		}
+		//this.npc = new app.NPC();
 		window.addEventListener("keypress", this.handleKeys.bind(this), true);
 	},
 
+
 	update: function(){
 		console.log("Game Screen: update");
-		//this.npc.graphic.position.x++;
-		//this.npc.move();
-		this.npc.moveAI();
-		//console.log("GameScreen: update");
+		
+		//Ai movement
+		for(var i = 0; i < app.people.length; i++){
+			app.people[i].moveAI(this.player.graphic.position.x, this.player.graphic.position.y);
+		}
 	},
 
 	exit: function(){
@@ -26,7 +35,7 @@ app.GameScreen = {
 	},
 
 	handleKeys: function(e){
-		//console.log(e.keyCode);
+		console.log(e.keyCode);
 		if(e.keyCode === 100){
 			//console.log("wut wut wut");
 			this.player.graphic.position.x += this.player.speed;
