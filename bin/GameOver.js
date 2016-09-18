6de92479c5d20e104b979f6c3e40de00d111b202
Mike <mike42092@gmail.com>
2016-09-17 23:41:45 -0400

    Added Game Over Text

--- bin/GameOver.js
+++ bin/GameOver.js
@@ -7,7 +7,9 @@ app.GameOver = {
 
 	init: function(stage){
 
-		console.log("HERE");
+		var text = new PIXI.Text('GAME OVER',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+		text.anchor = .5;
+		stage.addChild(text);
 		this.testButton = new app.Button(10, 10, 50, 50);
 		stage.addChild(this.testButton);
 		
