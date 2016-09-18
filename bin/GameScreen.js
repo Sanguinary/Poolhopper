af2d8eca6ea2122449d2706ed51b087f9c42ffde
AdamSJohnson <Adam.Johnson.main@gmail.com>
2016-09-18 02:44:40 -0400

    added more baddies

--- bin/GameScreen.js
+++ bin/GameScreen.js
@@ -22,6 +22,12 @@ app.GameScreen = {
 			app.people[i].graphic.position.y += i*100;
                         app.people[i].graphic.position.x += 10;
 		}
+                
+                for(var i = 5; i < 10; i++){
+			app.people.push(new app.NPC());
+			app.people[i].graphic.position.y += (i-4)*100;
+                        app.people[i].graphic.position.x += 900;
+		}
 		//this.npc = new app.NPC();
 		window.addEventListener("keydown", this.handleKeysDown.bind(this), true);
 		window.addEventListener("keyup", this.handleKeysUp.bind(this), true);
